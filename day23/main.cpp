9252e3430e12de6206e13f7a75622ada7bf374f6
Erwan <erwan.delepinau@free.fr>
2020-12-24 15:37:50 +0100

    optimized day23 further
    got down to ~0.25s on my machine, using only a vector of ints (saw someone doing smth similar in C on reddit) instead of Cup objects

--- day23/main.cpp
+++ day23/main.cpp
@@ -8,40 +8,40 @@
 #include <utility>
 #include <vector>
 
-struct Cup {
-    int value;
-    Cup* following;
+// struct Cup {
+//     int value;
+//     Cup* following;
 
-    Cup() : value(0) {}
-    Cup(int value) : value(value) {}
-};
+//     Cup() : value(0) {}
+//     Cup(int value) : value(value) {}
+// };
 
-void printCups(Cup* firstCup) {
-    Cup* nextCup = firstCup->following;
-    std::cout << firstCup->value;
-    while (nextCup != firstCup) {
-        std::cout << " -> " << nextCup->value;
-        nextCup = nextCup->following;
-    }
-    std::cout << std::endl;
-}
+// void printCups(Cup* firstCup) {
+//     Cup* nextCup = firstCup->following;
+//     std::cout << firstCup->value;
+//     while (nextCup != firstCup) {
+//         std::cout << " -> " << nextCup->value;
+//         nextCup = nextCup->following;
+//     }
+//     std::cout << std::endl;
+// }
 
 int mod(int a, int b) {
     int ret = a % b;
     return ret >= 0 ? ret : ret + b;
 }
 
-int decrementLabel(int label) { return mod(label - 2, 1'000'000) + 1; }
+int decrementCup(int cup) { return mod(cup - 2, 1'000'000) + 1; }
 
-int getDestinationLabel(int currentLabel, Cup* firstOf3Cups) {
-    int destinationLabel = decrementLabel(currentLabel);
-    while (firstOf3Cups->value == destinationLabel ||
-           firstOf3Cups->following->value == destinationLabel ||
-           firstOf3Cups->following->following->value == destinationLabel) {
-        destinationLabel = decrementLabel(destinationLabel);
+int getDestinationCup(int currentCup, const std::vector<int>& cups) {
+    int destinationCup = decrementCup(currentCup);
+
+    while (cups[currentCup] == destinationCup || cups[cups[currentCup]] == destinationCup ||
+           cups[cups[cups[currentCup]]] == destinationCup) {
+        destinationCup = decrementCup(destinationCup);
     }
 
-    return destinationLabel;
+    return destinationCup;
 }
 
 int main(int argc, char* argv[]) {
@@ -53,66 +53,57 @@ int main(int argc, char* argv[]) {
     inputFile.close();
 
     // Using a vector instead of a map to store cups for faster access
-    std::vector<Cup> cups(1'000'000);
+    // Size 1'000'001 so that we don't have to use (value - 1) as index each time
+    // The place at index 0 is unused
+    std::vector<int> cups(1'000'001);
 
-    Cup* previousCup{nullptr};
-    Cup* firstCup{nullptr};
+    int previousCup;
+    int firstCup;
 
     // Initializing the cups contained in the input
     for (int i = 0; i < buffer.size(); ++i) {
         int value = static_cast<int>(buffer[i] - '0');
-        cups[value - 1] = Cup(value);
-        Cup* cupPtr = &(cups[value - 1]);
 
         if (i == 0) {
-            firstCup = cupPtr;
+            firstCup = value;
         }
         if (i > 0) {
-            previousCup->following = cupPtr;
+            cups[previousCup] = value;
         }
 
-        previousCup = cupPtr;
+        previousCup = value;
     }
 
     // Initializing the other cups from 10 to 1'000'000
     for (int i = 10; i <= 1'000'000; ++i) {
-        cups[i - 1] = Cup(i);
-        Cup* cupPtr = &(cups[i - 1]);
-
-        if (i > 0) {
-            previousCup->following = cupPtr;
-        }
-
-        previousCup = cupPtr;
+        cups[previousCup] = i;
+        previousCup = i;
     }
-    cups[1'000'000 - 1].following = firstCup;
+    cups[1'000'000] = firstCup;
 
     // Playing the actual 10'000'000 rounds
-    Cup* currentCup = firstCup;
+    int currentCup = firstCup;
     for (int i = 0; i < 10'000'000; ++i) {
         // std::cout << "Before move " << i << ": ";
         // printCups(currentCup);
 
-        // Removing the 3 cups after the currentCup from the circle
-        Cup* firstOf3Cups = currentCup->following;
-        Cup* lastOf3Cups = currentCup->following->following->following;
+        // Choosing destination cup and removing the 3 cups after the currentCup from the circle
+        int firstOf3Cups = cups[currentCup];
+        int lastOf3Cups = cups[cups[firstOf3Cups]];
 
-        currentCup->following = lastOf3Cups->following;
+        int destinationCup = getDestinationCup(currentCup, cups);
 
-        // Choosing destination cup
-        int destinationLabel = getDestinationLabel(currentCup->value, firstOf3Cups);
-        Cup* destinationCup = &(cups[destinationLabel - 1]);
+        cups[currentCup] = cups[lastOf3Cups];
 
         // Reinserting them after the destinationCup
-        lastOf3Cups->following = destinationCup->following;
-        destinationCup->following = firstOf3Cups;
+        cups[lastOf3Cups] = cups[destinationCup];
+        cups[destinationCup] = firstOf3Cups;
 
-        currentCup = currentCup->following;
+        currentCup = cups[currentCup];
     }
 
-    Cup cupOne = cups[0];
     // The cast is needed to prevent integer overflow with int multiplication
-    long result = static_cast<long>(cupOne.following->value) * cupOne.following->following->value;
+    long result = static_cast<long>(cups[1]) * cups[cups[1]];
 
     std::cout << "Product of the two cups clockwise of cup 1: " << result << std::endl;
 
